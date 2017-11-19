0bf46b5433dcfb7dd60fa13679f6582472bf34e9
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2017-11-19 19:36:41 +0100

    changed bot Index

--- TelegramSecretaryBotApp/Controllers/HomeController.cs
+++ TelegramSecretaryBotApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TelegramSecretaryBotApp.Controllers
     {
         public string Index()
         {
-            return "My best bot ever !";
+            return "Test message. My bot is the best bot ever !";
         }
 
         /*
